4ecd9daeb0c7fe1c97cb88356fb201403faa4459
koinotice <koinotice@gmail.com>
2019-03-16 16:53:57 +0800

    update_credit_link

--- app.go
+++ app.go
@@ -12,7 +12,7 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	store "./contracts" // for demo
+	"./contracts" // for demo
 )
 
 func main() {
@@ -21,7 +21,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	privateKey, err := crypto.HexToECDSA("9a82accbc285082eebde4a69249738b5aedccb2008e14d2591bd0801de695515")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -49,28 +49,31 @@ func main() {
 	auth.GasLimit = uint64(300000) // in units
 	auth.GasPrice = gasPrice
 
-	address := common.HexToAddress("0x147B8eb97fD247D06C4006D269c90C1908Fb5D54")
-	instance, err := store.NewStore(address, client)
+	address := common.HexToAddress("0x4F1A733fe1077F591caf40dd3fC46044D13463cA")
+	instance, err := hashdice.NewHashDice(address, client)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	key := [32]byte{}
-	value := [32]byte{}
-	copy(key[:], []byte("foo"))
-	copy(value[:], []byte("bar"))
+	//key := [32]byte{}
+	//value := [32]byte{}
+	//copy(key[:], []byte("foo"))
+	//copy(value[:], []byte("bar"))
 
-	tx, err := instance.SetItem(auth, key, value)
+	big1 := new(big.Int).SetUint64(uint64(1))
+
+	tx, err := instance.SetRoundWait(auth, big1)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf("tx sent: %s", tx.Hash().Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
 
-	result, err := instance.Items(nil, key)
+	result, err := instance.GetRoundWait(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(result[:])) // "bar"
+	 fmt.Println((result)) // "bar"
+
 }
